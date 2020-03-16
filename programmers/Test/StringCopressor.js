0bd4202920c3a0479c6c4f02055d3688f49a3484
sugrace <sugrace3000@gmail.com>
2020-03-16 14:06:13 +0900

    Programmers StringCompressor

--- programmers/Test/StringCopressor.js
+++ programmers/Test/StringCopressor.js
@@ -1,9 +1,30 @@
 function solution(s) {
-    var answer = 0;
     let maxDiv = parseInt(s.length / 2);
-  
-    console.log(maxDiv)
-    return answer;
+    let min = s.length;
+    for(let i=1; i<=maxDiv; i++){
+        let count = 1;
+        let temp = '';
+        let string = s.substring(0,i)
+        for(let j=i; j < s.length; j+=i){
+            let slice = s.substring(j,i+j);
+            if(string !== slice){
+                temp += count > 1 ? count + string : string;
+                string = slice;
+                count = 1;
+            }else{
+                count++;
+            }
+        }
+        if(string){
+            temp += count > 1 ? count + string : string;
+        }
+      
+        if(min >= temp.length){
+            min = temp.length; 
+        }
+      
+    }
+    return min;
 }
 
-solution("skjfksdfjdskl")
+solution("aabbaccc")
